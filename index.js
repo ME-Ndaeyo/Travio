3b830e441407c86fe4f4548d0fb335ae004ab55c
Bobbiie <mmeyenendaeyo@gmail.com>
2022-07-27 20:39:14 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const navSlide = () =>{
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.navbar');
+    const close = document.querySelector('.close');
+
+    menu.addEventListener('click', function(){
+        if(menu.style.display = 'block'){
+            nav.classList.toggle('show');
+            menu.style.display = "none";
+            close.style.display = "block"
+        }
+    });
+    close.addEventListener('click', function(){
+        if(close.style.display = 'block'){
+            nav.classList.toggle('show');
+            close.style.display = "none";
+            menu.style.display = "block"
+        }
+    });
+}
+
+navSlide();
